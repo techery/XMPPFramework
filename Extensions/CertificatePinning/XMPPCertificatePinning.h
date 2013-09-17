82e60a8166c471925ef5dfeda6137b84d09b80cd
David Chiles <dwalterc@gmail.com>
2013-09-17 15:51:38 -0700

    added AFNetworking for cert pinning and verifcation

---
+++ Extensions/CertificatePinning/XMPPCertificatePinning.h
@@ -0,0 +1,13 @@
+//
+//  XMPPCertificatePinning.h
+//  Off the Record
+//
+//  Created by David on 9/17/13.
+//  Copyright (c) 2013 Chris Ballinger. All rights reserved.
+//
+
+#import "XMPPModule.h"
+
+@interface XMPPCertificatePinning : XMPPModule
+
+@end
---
+++ Extensions/CertificatePinning/XMPPCertificatePinning.m
@@ -0,0 +1,13 @@
+//
+//  XMPPCertificatePinning.m
+//  Off the Record
+//
+//  Created by David on 9/17/13.
+//  Copyright (c) 2013 Chris Ballinger. All rights reserved.
+//
+
+#import "XMPPCertificatePinning.h"
+
+@implementation XMPPCertificatePinning
+
+@end
