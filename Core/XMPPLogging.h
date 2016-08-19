2396664d66c0df090959d2af6265b811f2cd9b9f
Sergey Zenchenko <sergeizenchenko@gmail.com>
2016-08-19 13:32:22 +0300

    Working on carthage support.

--- Core/XMPPLogging.h
+++ Core/XMPPLogging.h
@@ -1,65 +1,65 @@
 /**
  * In order to provide fast and flexible logging, this project uses Cocoa Lumberjack.
- * 
+ *
  * The GitHub project page has a wealth of documentation if you have any questions.
  * https://github.com/robbiehanson/CocoaLumberjack
- * 
+ *
  * Here's what you need to know concerning how logging is setup for XMPPFramework:
- * 
+ *
  * There are 4 log levels:
  * - Error
  * - Warning
  * - Info
  * - Verbose
- * 
+ *
  * In addition to this, there is a Trace flag that can be enabled.
  * When tracing is enabled, it spits out the methods that are being called.
- * 
+ *
  * Please note that tracing is separate from the log levels.
  * For example, one could set the log level to warning, and enable tracing.
- * 
+ *
  * All logging is asynchronous, except errors.
  * To use logging within your own custom files, follow the steps below.
- * 
+ *
  * Step 1:
  * Import this header in your implementation file:
- * 
+ *
  * #import "XMPPLogging.h"
- * 
+ *
  * Step 2:
  * Define your logging level in your implementation file:
- * 
+ *
  * // Log levels: off, error, warn, info, verbose
  * static const int xmppLogLevel = XMPP_LOG_LEVEL_VERBOSE;
- * 
+ *
  * If you wish to enable tracing, you could do something like this:
- * 
+ *
  * // Log levels: off, error, warn, info, verbose
  * static const int xmppLogLevel = XMPP_LOG_LEVEL_INFO | XMPP_LOG_FLAG_TRACE;
- * 
+ *
  * Step 3:
  * Replace your NSLog statements with XMPPLog statements according to the severity of the message.
- * 
+ *
  * NSLog(@"Fatal error, no dohickey found!"); -> XMPPLogError(@"Fatal error, no dohickey found!");
- * 
+ *
  * XMPPLog has the same syntax as NSLog.
  * This means you can pass it multiple variables just like NSLog.
- * 
+ *
  * You may optionally choose to define different log levels for debug and release builds.
  * You can do so like this:
- * 
+ *
  * // Log levels: off, error, warn, info, verbose
  * #if DEBUG
  *   static const int xmppLogLevel = XMPP_LOG_LEVEL_VERBOSE;
  * #else
  *   static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
  * #endif
- * 
+ *
  * Xcode projects created with Xcode 4 automatically define DEBUG via the project's preprocessor macros.
  * If you created your project with a previous version of Xcode, you may need to add the DEBUG macro manually.
-**/
+ **/
 
-#import "DDLog.h"
+#import <CocoaLumberjack/CocoaLumberjack.h>
 
 // Global flag to enable/disable logging throughout the entire xmpp framework.
 
@@ -88,7 +88,7 @@
 
 // Setup fine grained logging.
 // The first 4 bits are being used by the standard log levels (0 - 3)
-// 
+//
 // We're going to add tracing, but NOT as a log level.
 // Tracing can be turned on and off independently of log level.
 
@@ -122,48 +122,48 @@
 // These are primarily wrappers around the macros defined in Lumberjack's DDLog.h header file.
 
 #define XMPP_LOG_OBJC_MAYBE(async, lvl, flg, ctx, frmt, ...) \
-    do{ if(XMPP_LOGGING_ENABLED) LOG_MAYBE(async, lvl, flg, ctx, sel_getName(_cmd), frmt, ##__VA_ARGS__); } while(0)
+do{ if(XMPP_LOGGING_ENABLED) LOG_MAYBE(async, lvl, flg, ctx, nil, sel_getName(_cmd), frmt, ##__VA_ARGS__); } while(0)
 
 #define XMPP_LOG_C_MAYBE(async, lvl, flg, ctx, frmt, ...) \
-    do{ if(XMPP_LOGGING_ENABLED) LOG_MAYBE(async, lvl, flg, ctx, __FUNCTION__, frmt, ##__VA_ARGS__); } while(0)
+do{ if(XMPP_LOGGING_ENABLED) LOG_MAYBE(async, lvl, flg, ctx, nil, __FUNCTION__, frmt, ##__VA_ARGS__); } while(0)
 
 
 #define XMPPLogError(frmt, ...)    XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_ERROR,   xmppLogLevel, XMPP_LOG_FLAG_ERROR,  \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 #define XMPPLogWarn(frmt, ...)     XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_WARN,    xmppLogLevel, XMPP_LOG_FLAG_WARN,   \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 #define XMPPLogInfo(frmt, ...)     XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_INFO,    xmppLogLevel, XMPP_LOG_FLAG_INFO,    \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 #define XMPPLogVerbose(frmt, ...)  XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_VERBOSE, xmppLogLevel, XMPP_LOG_FLAG_VERBOSE, \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 #define XMPPLogTrace()             XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_TRACE,   xmppLogLevel, XMPP_LOG_FLAG_TRACE, \
-                                                  XMPP_LOG_CONTEXT, @"%@: %@", THIS_FILE, THIS_METHOD)
+XMPP_LOG_CONTEXT, @"%@: %@", THIS_FILE, THIS_METHOD)
 
 #define XMPPLogTrace2(frmt, ...)   XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_TRACE,   xmppLogLevel, XMPP_LOG_FLAG_TRACE, \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 
 #define XMPPLogCError(frmt, ...)      XMPP_LOG_C_MAYBE(XMPP_LOG_ASYNC_ERROR,   xmppLogLevel, XMPP_LOG_FLAG_ERROR,   \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 #define XMPPLogCWarn(frmt, ...)       XMPP_LOG_C_MAYBE(XMPP_LOG_ASYNC_WARN,    xmppLogLevel, XMPP_LOG_FLAG_WARN,    \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 #define XMPPLogCInfo(frmt, ...)       XMPP_LOG_C_MAYBE(XMPP_LOG_ASYNC_INFO,    xmppLogLevel, XMPP_LOG_FLAG_INFO,    \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 #define XMPPLogCVerbose(frmt, ...)    XMPP_LOG_C_MAYBE(XMPP_LOG_ASYNC_VERBOSE, xmppLogLevel, XMPP_LOG_FLAG_VERBOSE, \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 #define XMPPLogCTrace()               XMPP_LOG_C_MAYBE(XMPP_LOG_ASYNC_TRACE,   xmppLogLevel, XMPP_LOG_FLAG_TRACE, \
-                                                  XMPP_LOG_CONTEXT, @"%@: %s", THIS_FILE, __FUNCTION__)
+XMPP_LOG_CONTEXT, @"%@: %s", THIS_FILE, __FUNCTION__)
 
 #define XMPPLogCTrace2(frmt, ...)     XMPP_LOG_C_MAYBE(XMPP_LOG_ASYNC_TRACE,   xmppLogLevel, XMPP_LOG_FLAG_TRACE, \
-                                                  XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
+XMPP_LOG_CONTEXT, frmt, ##__VA_ARGS__)
 
 // Setup logging for XMPPStream (and subclasses such as XMPPStreamFacebook)
 
@@ -182,10 +182,10 @@
 #define XMPP_LOG_ASYNC_RECV_POST (YES && XMPP_LOG_ASYNC_ENABLED)
 
 #define XMPPLogSend(format, ...)     XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_SEND, xmppLogLevel, \
-                                                XMPP_LOG_FLAG_SEND, XMPP_LOG_CONTEXT, format, ##__VA_ARGS__)
+XMPP_LOG_FLAG_SEND, XMPP_LOG_CONTEXT, format, ##__VA_ARGS__)
 
 #define XMPPLogRecvPre(format, ...)  XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_RECV_PRE, xmppLogLevel, \
-                                                XMPP_LOG_FLAG_RECV_PRE, XMPP_LOG_CONTEXT, format, ##__VA_ARGS__)
+XMPP_LOG_FLAG_RECV_PRE, XMPP_LOG_CONTEXT, format, ##__VA_ARGS__)
 
 #define XMPPLogRecvPost(format, ...) XMPP_LOG_OBJC_MAYBE(XMPP_LOG_ASYNC_RECV_POST, xmppLogLevel, \
-                                                XMPP_LOG_FLAG_RECV_POST, XMPP_LOG_CONTEXT, format, ##__VA_ARGS__)
+XMPP_LOG_FLAG_RECV_POST, XMPP_LOG_CONTEXT, format, ##__VA_ARGS__)
--- DynamicFramework/Carthage/Build/iOS/XMPPFramework.framework/Headers/XMPPFramework.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for XMPPFramework.
-FOUNDATION_EXPORT double XMPPFrameworkVersionNumber;
-
-//! Project version string for XMPPFramework.
-FOUNDATION_EXPORT const unsigned char XMPPFrameworkVersionString[];
-
-#import "XMPP.h"
